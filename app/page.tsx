06851d8362a55a8945ee9e47921692df73d8207c
RAFAEL MODESTO <rafaelmodesto@RAFAELs-iMac.local>
2023-12-02 21:35:57 -0500

    feat: added logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
+import AcmeLogo from '@/app/ui/acme-logo';
 
-function AcmeLogo() {
-  // Add your AcmeLogo component implementation here
-  return <div>Acme Logo</div>;
-}
-
-export { AcmeLogo };
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
@@ -16,7 +11,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -24,11 +19,11 @@ export default function Page() {
         <p
       className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to StatData.</strong> This is an example of a{' '}
+            <a href="https://github.com/Rmodesto" className="text-blue-500">
+             Simple Dashboard
             </a>
-            , brought to you by Vercel.
+            , brought to you by Rafael.
           </p>
           <Link
             href="/login"
