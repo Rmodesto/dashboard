d8fec58606d8ac67b6a433f33ecff9c5ddbc37b9
Rafael Modesto <1rafaelmodesto@gmail.com>
2023-12-10 23:58:34 -0500

    updated logo text/icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import AcmeLogo from '@/app/ui/acme-logo';
+import AcmeLogo from '@/app/ui/StarData';
 
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
--- app/ui/StarData.tsx
+++ app/ui/StarData.tsx
@@ -1,14 +1,19 @@
 // npm ls @/app/ui/fonts
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+import Stars from '/public/magicstars.png'
 
-export default function AcmeLogo() {
+
+
+export default function StarData() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px] ">Acme</p>
+     <Image src={Stars} width={55} height={55} alt="star-emoji-white"/>
+      <p className="text-[40px] ">StarData</p>
+      
     </div>
   );
 }
