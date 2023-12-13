e47d4f8758b956f9e691cc71d41e849b67942434
Rafael Modesto <1rafaelmodesto@gmail.com>
2023-12-12 22:44:56 -0500

    improved accessibility

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
-
+import { z } from 'zod';
 
 import {
   CustomerField,
@@ -13,7 +13,19 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
+});
 
 
 export async function fetchRevenue() {
