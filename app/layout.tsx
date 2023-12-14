644e963517d3b7269353f4d88142ab4ac41a0857
Rafael Modesto <1rafaelmodesto@gmail.com>
2023-12-13 19:13:15 -0500

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: {
+    template: '%s | StarData Dashboard',
+    default: 'StarData Dashboard',
+  },
+  description: 'Simple Dashboard.',
+  metadataBase: new URL('https://dashboard-bomj76hth-rmodesto.vercel.app/'),
+};
+
 export default function RootLayout({
   children,
 }: {
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import styles from '@/app/ui/home.module.css';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default function Page() {
   return (
